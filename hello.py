a0040a8b09c9bf36645c802ab11ccbab82e40986
jp-fatflow <125776732+jp-fatflow@users.noreply.github.com>
2023-02-19 10:17:55 +0700

    เพิ่ม list freinds ลุงตู่

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 print('Hello Uncle')
+
 print('Hello Somchai')
+
 print('Hello Github')
-friend=['Loong','Pa Lek','Loong Dam']
+
+friend=['Loong','Pa Lek','Loong Dam','Loong Too']
+
 print(friend[0])
